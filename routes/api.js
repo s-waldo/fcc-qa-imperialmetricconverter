d91422bb9e1f76b64602ef9f4d410f0ab5607239
s-waldo <stephen.waldo.94@gmail.com>
2024-10-14 12:35:46 -0500

    Set API links

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,38 @@
-'use strict';
+"use strict";
 
-const expect = require('chai').expect;
-const ConvertHandler = require('../controllers/convertHandler.js');
+const expect = require("chai").expect;
+const ConvertHandler = require("../controllers/convertHandler.js");
 
 module.exports = function (app) {
-  
   let convertHandler = new ConvertHandler();
 
+  app.get("/api/convert", (req, res) => {
+    const userInput = req.query.input;
+    const initNum = convertHandler.getNum(userInput);
+    const initUnit = convertHandler.getUnit(userInput);
+    const returnUnit = convertHandler.getReturnUnit(initUnit);
+    const returnNum = convertHandler.convert(initNum, initUnit);
+    const string = convertHandler.getString(
+      initNum,
+      convertHandler.spellOutUnit(initUnit),
+      returnNum,
+      convertHandler.spellOutUnit(returnUnit)
+    )
+    if (!initNum || !initUnit || !returnNum || !returnUnit) {
+      if (!initNum && !initUnit) {
+        res.send('invalid number and unit')
+        return
+      }
+      if (!initNum) res.send('invalid number')
+      if (!initUnit) res.send('invalid unit')
+      return
+    }
+    res.json({
+      initNum,
+      initUnit,
+      returnNum,
+      returnUnit,
+      string
+    });
+  });
 };
